ed78b2b527572d0e5e8b9e8fd3aab2f66ab0c779
Alexis Panyathong <alexispanyathong@gmail.com>
2019-10-16 14:53:45 -0500

    Paging and Sorting completed.

--- school/src/main/java/com/lambdaschool/school/controller/CourseController.java
+++ school/src/main/java/com/lambdaschool/school/controller/CourseController.java
@@ -1,14 +1,19 @@
 package com.lambdaschool.school.controller;
 
 import com.lambdaschool.school.model.Course;
+import com.lambdaschool.school.model.Student;
 import com.lambdaschool.school.service.CourseService;
-import com.lambdaschool.school.view.CountStudentsInCourses;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiImplicitParams;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "/courses")
@@ -17,6 +22,27 @@ public class CourseController
     @Autowired
     private CourseService courseService;
 
+    @ApiImplicitParams({@ApiImplicitParam(name = "page",
+            dataType = "integer", paramType = "query",
+            value = "Results page you want to retrieve (0..N)"),
+            @ApiImplicitParam(name = "size", dataType = "integer",
+                    paramType = "query", value = "Number of records per page."),
+            @ApiImplicitParam(name = "sort", allowMultiple = true, dataType = "string",
+                    paramType = "query", value = "Sorting criteria in for format: property(,asc|desc.). " +
+                    "Default sort order is ascending. " +
+                    "Multiple sort criteria are supported.")})
+
+    // paging and sorting
+
+    // localhost:2019/students/students/paging/?page=1&size=10
+    @GetMapping(value = "/courses/paging",
+            produces = {"application/json"})
+    public ResponseEntity<?> ListAllCoursesByPage(@PageableDefault(page = 1,
+            size = 3) Pageable pageable)
+    {                        // findAllPageable(pageable.unpaged()) <- returns everything
+        List<Course> myCourses = courseService.findAllPageable(pageable);
+        return new ResponseEntity<>(myCourses, HttpStatus.OK);
+    }
     // GET request http://localhost:2019/courses
     @GetMapping(value = "/courses", produces = {"application/json"})
     public ResponseEntity<?> listAllCourses()
